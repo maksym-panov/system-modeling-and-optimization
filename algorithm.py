5113aaaa72fd3af296a4a05af628705bce833b81
maksym-panov <work.maksym.panov@gmail.com>
2023-09-16 20:20:06 +0300

    Commented functions of algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,26 +1,32 @@
 import matplotlib.pyplot as plt
 import math
 
+# Receives a vector of random variable values 
+# divides them into intervals of equal size
+# and draws a relative frequencies polygon
 def buildRelativeFrequenciesPolygon(vector):
   vector = sorted(vector)
   x = evalArgumentsVector(vector)
   y = evalRelativeFreqsVector(vector)
-
   plt.plot(x, y, "ko")
   plt.title("Relative frequencies polygon")
   plt.xlabel("Random variable value")
   plt.ylabel("Relative frequency")
   plt.show()
 
+# Returns a list of midpoints of the intervals
 def evalArgumentsVector(vector):
   [_, k, inter] = evalNumericParams(vector)
   min = vector[0]
   return [( (min + inter * i) + (min + inter * (i + 1)) ) / 2 for i in range(k)]
 
+# Returns a list of frequencies for each interval
 def evalRelativeFreqsVector(vector): 
   [_, k, inter] = evalNumericParams(vector) 
   return [relFreqOnInterval(vector, inter, i) for i in range(k)]
 
+# Helper function that evaluates a 
+# frequency for a specific interval
 def relFreqOnInterval(vector, inter, i):
   left = vector[0] + inter * i
   right = left + inter
@@ -30,6 +36,8 @@ def relFreqOnInterval(vector, inter, i):
       count += 1
   return count / len(vector)
 
+# Returns list of vector lenght, 
+# k parameter and interval length
 def evalNumericParams(vector):
   l = len(vector)
   k = math.ceil(l / 10)
